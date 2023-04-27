b016927833fba7ee1575cd23409ea693f98e6e2e
Ubuntu <ubuntu@ip-172-31-18-239.eu-central-1.compute.internal>
2023-04-27 06:01:31 +0000

    alpha flask version + updated db_functions.py file

--- Code/Backend/Python/app.py
+++ Code/Backend/Python/app.py
@@ -1,5 +1,66 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from db_functions import get_floor_data, insert_student_submission
+from datetime import datetime, timedelta
+
 
 app = Flask(__name__)
 api = Api(app)
+
+
+class floor_one(Resource):
+    def get(self):
+        data = get_floor_data(1)
+        return data
+
+
+class floor_two(Resource):
+    def get(self):
+        data = get_floor_data(2)
+        return data
+
+
+class floor_three(Resource):
+    def get(self):
+        data = get_floor_data(3)
+        return data
+
+
+class floor_four(Resource):
+    def get(self):
+        data = get_floor_data(4)
+        return data
+
+
+class submit_form(Resource):
+    def post(self):
+        floor = request.args.get("floor_id")
+        table_num = request.args.get("table_id")
+        id = request.args.get("id")
+        email = request.args.get("email")
+        time = request.args.get("time")
+        time_splitted = time.split(':')
+        hours = time_splitted[0]
+        minutes = time_splitted[1]
+        current_time = timedelta.now()
+        stay_interval = timedelta(hours=hours, minutes=minutes)
+        leave_time = current_time + stay_interval
+        insert_student_submission(id=id,
+                                  email=email,
+                                  leave_time=leave_time,
+                                  floor=floor,
+                                  table_num=table_num)
+
+
+class extend_stay(Resource):
+    def post(self):
+        pass
+
+
+api.add_resource(floor_one, "/f1")
+api.add_resource(floor_two, "/f2")
+api.add_resource(floor_three, "/f3")
+api.add_resource(floor_four, "/f4")
+api.add_resource(submit_form, "/submit")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
--- Code/Backend/Python/db_functions.py
+++ Code/Backend/Python/db_functions.py
@@ -15,7 +15,7 @@ def connect_to_db():
     return cursor, connection
 
 
-def generic_query(query, fetch_one=False):
+def generic_query(query: str, fetch_one=False):
     cursor, connection = connect_to_db()
     query = query
     cursor.execute(query)
@@ -23,6 +23,18 @@ def generic_query(query, fetch_one=False):
         res = cursor.fetchone()
     else:
         res = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+    return res
+
+
+def generic_change_query(query: str):
+    cursor, connection = connect_to_db()
+    query = query
+    res = cursor.execute(query)
+    connection.commit()
+    cursor.close()
     connection.close()
     return res
 
@@ -50,11 +62,22 @@ def get_students_to_send_mail():
     return res
 
 
-def update_student_is_reminded(id):
+def update_student_is_reminded(id: int):
     query = f"""
-    UPDATE Students
+    UPDATE Library.Students
     SET IsReminded = 1
     WHERE ID = {id}
     """
-    res = generic_query(query)
+    res = generic_change_query(query)
     return res
+
+
+def insert_student_submission(id: int,
+                              email: str,
+                              leave_time: datetime,
+                              floor: int,
+                              table_num: int):
+    query = f"""
+    INSERT INTO Students (ID, Email, EntryTime, LeaveTime, Floor, Table_num, IsReminded)
+    VALUES ({id}, {email}, NOW(), {leave_time}, {floor}, {table_num}, 0)
+    """
