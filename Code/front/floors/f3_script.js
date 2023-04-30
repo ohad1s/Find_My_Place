0f314a14cfd93a8937485835cb00db43332bda4d
dvirBiton1 <dvir199710@gmail.com>
2023-04-30 11:55:59 +0300

    floors front

--- Code/front/floors/f3_script.js
+++ Code/front/floors/f3_script.js
@@ -2,6 +2,7 @@ SERVER = "http://localhost:5000"
 
 function generatePage(data) {
     // Create the table HTML content
+    let title = '<h1 class="title">Floor 3</h1>'
     let tableHtml = '<table class="cool-table">\n' +
         '<thead>\n' +
         '<tr><th>Table Number</th><th>Current Students</th><th>Max Students</th></tr>\n' +
@@ -17,13 +18,13 @@ function generatePage(data) {
     tableHtml += '</tbody>\n</table>\n';
 
     // Create the photo HTML content
-    const photoHtml = '<div><img src="#" alt="A photo"></div>\n';
+    const photoHtml = '<div><img src="floor1_map.png" alt="A photo"></div>\n';
 
     // Create the "Return" button HTML content
-    const buttonHtml = '<div><button class="return-button" onclick="window.location.href=\'../index.html\';">Return</button></div>\n';
+    const buttonHtml = '<div class="sidenav"><button class="home-button" onclick="window.location.href=\'../index.html\';">Home</button></div>\n';
 
     // Combine the table, photo, and button HTML into a single string
-    const html = tableHtml + photoHtml + buttonHtml;
+    const html = title + tableHtml + photoHtml + buttonHtml;
 
     // Return the HTML content as a string
     return html;
@@ -35,6 +36,21 @@ function fetchTemplate(pageName) {
         .then(data => {
             const html = generatePage(data);
             document.body.innerHTML = html;
+            color_table();
         })
         .catch(error => console.error(error));
+}
+function color_table() {
+    const rows = document.querySelectorAll('table tbody tr');
+    rows.forEach(row => {
+        const maxSeats = parseInt(row.cells[2].textContent);
+        const seatsTaken = parseInt(row.cells[1].textContent);
+        if (maxSeats <= seatsTaken) {
+            row.classList.remove('table-empty');
+            row.classList.add('table-full');
+        } else {
+            row.classList.remove('table-full');
+            row.classList.add('table-empty');
+        }
+    });
 }
