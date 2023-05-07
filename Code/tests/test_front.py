af22cd00e28e85dcc9bd3431da18b170442c8f8b
dvirBiton1 <dvir199710@gmail.com>
2023-05-07 18:20:34 +0300

    tests

--- Code/tests/test_front.py
+++ Code/tests/test_front.py
@@ -11,6 +11,7 @@ class PythonOrgSearch(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome(r"C:\Users\dvir1\webdriver\chromedriver")
 
+    @unittest.skip("")
     def test_index(self):
         driver = self.driver
         driver.get("http://findmyplace.online/")
@@ -26,7 +27,8 @@ class PythonOrgSearch(unittest.TestCase):
         self.assertEqual(selected_option.text, "Floor 2")
         self.assertEqual(selected_option.get_attribute("value"), "f2")
 
-    def test_submit(self):
+    @unittest.skip("skipping test_submit because it's not implemented yet")
+    def test_floor(self):
         driver = self.driver
         driver.get("http://findmyplace.online/floors/f1_page.html")
         self.assertIn("Floor 1", driver.title)
@@ -36,6 +38,38 @@ class PythonOrgSearch(unittest.TestCase):
         self.assertIn("Floor 3", driver.title)
         driver.get("http://findmyplace.online/floors/f4_page.html")
         self.assertIn("Floor 4", driver.title)
+
+    def test_extend(self):
+        driver = self.driver
+        driver.get("http://findmyplace.online/extend.html")
+        # self.assertIn("Extend", driver.title)
+        no_button = driver.find_element(By.ID, "no-button")
+        assert no_button.is_displayed() and no_button.is_enabled()
+        enter_button = driver.find_element(By.ID, "enter-button")
+        assert enter_button.is_displayed() and enter_button.is_enabled()
+        time_input = driver.find_element(By.ID, "time_to_add")
+        time_input.send_keys("01:30")
+        time_input.send_keys(Keys.RETURN)
+        input_field = driver.find_element(By.ID, "input-container2")
+        assert not input_field.is_displayed()
+
+    def test_submit(self):
+        driver = self.driver
+        driver.get("http://findmyplace.online/submit.html")
+        id_field = self.driver.find_element(By.ID, "ID")
+        email_field = self.driver.find_element(By.ID, "email")
+        time_field = self.driver.find_element(By.ID, "time")
+        submit_button = self.driver.find_element(By.ID, "submit-button")
+
+        # Leave all fields blank
+        submit_button.click()
+        try:
+            alert = driver.switch_to.alert
+            # If the above line executes without an exception, it means that an alert is present
+            self.assertTrue(True)
+            alert.dismiss()
+        except:
+            self.assertFalse(True, "Alert is not present")  # Assertion to fail the test
 
     def tearDown(self):
         self.driver.close()
